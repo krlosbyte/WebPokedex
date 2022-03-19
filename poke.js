ab30b6ad3289944cae052a2c6a127219fa7ff8cd
krlosbyte <52300844+krlosbyte@users.noreply.github.com>
2022-03-18 23:08:33 -0600

    Update poke.js

--- poke.js
+++ poke.js
@@ -1,44 +1,4 @@
-/*
-const fetchPokemon = () => {
-    const pokeName = document.getElementById("pokeName");
-    let pokeInput = pokeName.value.toLowerCase();
-    const url = `https://pokeapi.co/api/v2/pokemon/${pokeInput}`;
-    fetch(url).then((res) => {
-        if (res.status != "200") {
-            console.log(res);
-            pokeImage("./image.jpg");
-        }
-        else {
-            return res.json();
-        }
-    }).then((data) => {
-        console.log(data);
-        let pokeImg = data.sprites.front_default;
-        console.log(pokeImg);
-        pokeImage(pokeImg);
-    })
-}
 
-const pokeImage = (url) => {
-    const pokeImg = document.getElementById("pokeImg");
-    pokeImg.src = url;
-}
-*/
-
-/*
-const consulta = () => {
-    const url = `https://pokeapi.co/api/v2/pokemon/pikachu`;
-    fetch(url).then((respuesta) => {
-        return respuesta.json();
-    }).then((info) => {
-        console.log(info);
-        let pokeName = info.name;
-        console.log(pokeName);
-    })
-}
-
-consulta();
-*/
 
 const fetchPokemon = () => {
     const pokeName = document.getElementById("pokeName");
